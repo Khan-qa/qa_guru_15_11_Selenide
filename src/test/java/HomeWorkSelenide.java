0f474bbf419665e5a45bbcff446e47ec7e6dd47c
KKaiyrbekov <han_umag@mail.com>
2022-12-27 19:40:05 +0600

    test

--- src/test/java/HomeWorkSelenide.java
+++ src/test/java/HomeWorkSelenide.java
@@ -1,10 +1,8 @@
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
 
 import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
